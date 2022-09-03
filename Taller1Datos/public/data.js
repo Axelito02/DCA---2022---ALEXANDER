80d0a1b07af70adc086e5a3a8ad353fce4882ece
Alexander <98567345+Axelito02@users.noreply.github.com>
2022-09-03 07:01:39 -0500

    imagenes indepedientes de cada post

--- Taller1Datos/public/data.js
+++ Taller1Datos/public/data.js
@@ -6,7 +6,7 @@ const data = [
         "comment_count": "20", 
         "likes": "5,000",
         "date": "2",
-        "img": "./src/imgs/Profile.jpg"
+        "img": "./src/imgs/avogado.jfif"
 
     },
     {
@@ -16,7 +16,7 @@ const data = [
         "comment_count": "4", 
         "likes": "2,000",
         "date": "5",
-        "img": "./src/imgs/bg.jpg"
+        "img": "./src/imgs/avogado1.jpeg"
     },
     {
         "id": 3,
@@ -25,7 +25,7 @@ const data = [
         "comment_count": "12",
         "likes": "180,900",
         "date": "1",
-        "img": "./src/imgs/Profile.jpg"
+        "img": "./src/imgs/avogado2.jfif"
     },
     {
         "id": 4,
@@ -34,7 +34,7 @@ const data = [
         "comment_count": "5 ", 
         "likes": "200,450",
         "date": "10",
-        "img": "./src/imgs/Profile.jpg"
+        "img": "./src/imgs/avogado3.jfif"
     },
     {
         "id": 5,
@@ -43,7 +43,7 @@ const data = [
         "comment_count": "60", 
         "likes": "20,000",
         "date": "4",
-        "img": "./src/imgs/Profile.jpg"
+        "img": "./src/imgs/avogado4.jpg"
     },
     {
         "id": 6,
@@ -52,7 +52,7 @@ const data = [
         "comment_count": "12", 
         "likes": "5,000",
         "date": "7",
-        "img": "./src/imgs/Profile.jpg"
+        "img": "./src/imgs/avogado5.jpg"
     },
     {
         "id": 7,
@@ -61,7 +61,7 @@ const data = [
         "comment_count": "20", 
         "likes": "90,000",
         "date": "8",
-        "img": "./src/imgs/Profile.jpg"
+        "img": "./src/imgs/avogado6.jfif"
     },
     {
         "id": 8,
@@ -70,7 +70,7 @@ const data = [
         "comment_count": "2", 
         "likes": "230,000",
         "date": "14",
-        "img": "./src/imgs/Profile.jpg"
+        "img": "./src/imgs/avogado7.jfif"
     },
     {
         "id": 9,
@@ -79,7 +79,7 @@ const data = [
         "comment_count": "9", 
         "likes": "3,500",
         "date": "3",
-        "img": "./src/imgs/Profile.jpg"
+        "img": "./src/imgs/avogado8.jfif"
     },
     {
         "id": 10,
@@ -88,7 +88,7 @@ const data = [
         "comment_count": "32", 
         "likes": "450,000",
         "date": "15",
-        "img": "./src/imgs/Profile.jpg"
+        "img": "./src/imgs/avogado9.jfif"
     }
 ]
 
